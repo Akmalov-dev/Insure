1fa573af1a863ec5cb3c2341f142f5b871c703ca
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-05-01 13:08:16 +0500

    end of header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Heaer.css'
+import Family from '../header/images/family.png'
 
 const Header = () => {
   const [burger , setBurger] = useState(false);
   return (
-    <header className="header">
+   <>
+       <header className="header">
       <div className="logo">INSURE</div>
       <nav className="nav">
         <Link to="/home">HOW WE WORK</Link>
@@ -19,17 +21,27 @@ const Header = () => {
       {
        burger && (
         <div className='onclick'>
-          <button className='btn-login1' onClick={()=>setBurger(false)}><Link className='login'>X</Link></button>
+          <button className='btn-login1' onClick={()=>setBurger(false)}><Link className='login1'>X</Link></button>
           <input type="email"  placeholder='Email' className='email-input'/>
           <input type="password" placeholder='Password' className='input-password'/>
           <input type="date" className='input-date'/> 
           <input type="submit" className='btn-submit'/>
         </div>
        )
-
       }
-
     </header>
+
+    <div className='header-container'>
+      <div className='header-fonts'>
+        <p className='f-insurance'>Humanizing your insurance.</p>
+        <p className='f-get'>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.</p>
+        <button className='btn-plans'>view plans</button>
+      </div>
+      <div>
+        <img className='img-family' src={Family} alt="" />
+      </div>
+    </div>
+   </> 
   )
 }
 
